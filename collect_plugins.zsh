401cdb74342e1dc75b3ca2bc904d2bff776c1cf9
Evgeni Kunev <evgeni@kunev.me>
2014-04-11 13:40:46 +0300

    Collect ssh remotes properly as well

--- collect_plugins.zsh
+++ collect_plugins.zsh
@@ -4,6 +4,9 @@ echo 'typeset -A plugins'
 for dirname in *; do
     cd $dirname
     url=`git remote -v|ack --output='$1' '((https?|git)://.+)\s+\(fetch\)'`
+    if [[ -z $url ]]; then
+        url=`git remote -v|ack --output='$1' '(\w+@.+) \(fetch\)'`
+    fi
     cd - > /dev/null
 
     echo "plugins[${dirname}]='${url}'"
